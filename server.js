a96f5480d9d3af8999a84a39b1a8096faaf6cdbd
camilla-m <camilla-m@outlook.com>
2023-07-24 22:34:10 -0300

    change message from get nodejs cloud build - test 2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('teste 2 - tentando testar implantacao');
+  res.send('teste 2 - tentando testar implantacao com artifact registry');
 });
 
 app.listen(PORT, HOST, () => {
