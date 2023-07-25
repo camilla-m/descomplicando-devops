1779d833394e26da1d3326e6cd5d25a0e2317b65
camilla-m <camilla-m@outlook.com>
2023-07-25 14:26:53 -0300

    test dev

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('testando pipeline no kubernetes punk do devops');
+  res.send('testando pipeline no kubernetes punk do devops dois');
 });
 
 app.listen(PORT, HOST, () => {
